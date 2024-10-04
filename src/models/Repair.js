40b2d99cd51585ae616710cdb92fd2a47792236d
gabriel <pesoa01@gmail.com>
2024-10-04 16:36:49 -0300

    repair model started

--- src/models/Repair.js
+++ src/models/Repair.js
@@ -4,8 +4,16 @@ const sequelize = require('../utils/connection');
 const Repair = sequelize.define('repair', {
     moanobra: {
         type: DataTypes.REAL,
-        allowNull: true
+        allowNull: true,
+        defaultValue:0
+
     },
+    repuestos: {
+        type: DataTypes.REAL,
+        allowNull: true,
+        defaultValue:0
+        
+    }
 });
 
 module.exports = Repair;
